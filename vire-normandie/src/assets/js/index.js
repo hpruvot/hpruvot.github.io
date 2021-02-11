825576d4130139f27eaf02ba69cec1c595739437
helenep <helene.pruvot@adfab.fr>
2021-02-11 20:37:16 +0100

    Feature: Interview - Add behavior + 2 first steps integration

--- vire-normandie/src/assets/js/index.js
+++ vire-normandie/src/assets/js/index.js
@@ -1,13 +1,29 @@
+let interviewStep = 1;
+
 document.querySelectorAll('.zone__item').forEach(item => {
   item.addEventListener('click', () => {
     const index = item.getAttribute('data-zone');
-    console.log(index);
     document.querySelector('[data-popup="'+ index +'"]').classList.add('-isShowed');
+    if (index == 6) {
+      document.querySelector('[data-interview="1"]').classList.add('-isShowed')
+    }
   });
 })
 
 document.querySelectorAll('.popup__back').forEach(item => {
   item.addEventListener('click', () => {
     document.querySelector('.-isShowed').classList.remove('-isShowed');
+    interviewStep = 1;
+    document.querySelectorAll('.o-interview__tooltipList').forEach(interview => {
+      interview.classList.remove('-isShowed');
+    })
+  });
+});
+
+
+document.querySelector('.o-interview__tooltipNext').addEventListener('click', () => {
+  document.querySelectorAll('.o-interview__tooltipList').forEach(interview => {
+    interview.classList.remove('-isShowed');
   })
+  document.querySelector('[data-interview="'+ (interviewStep += 1) +'"]').classList.add('-isShowed');
 });
