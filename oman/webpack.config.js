541297df004a6e786b5496530c1c350c565f4f68
Helene Pruvot <helene.pruvot@adfab.fr>
2021-05-25 21:17:59 +0200

    Feature: Add fonts

--- oman/webpack.config.js
+++ oman/webpack.config.js
@@ -33,6 +33,14 @@ module.exports = (env, arg) => ({
             }
           }
         ]
+      },
+      {
+        test: /\.(woff2)$/,
+        loader: 'file-loader',
+        options: {
+					name: '[name].[ext]',
+          outputPath: 'fonts'
+        },
       }
     ]
   },
